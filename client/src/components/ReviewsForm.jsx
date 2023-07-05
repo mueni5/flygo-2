38df557c7d0fd900b2a50c9abf29c0a7b6f33080
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-07-05 23:55:50 +0300

    editted the review form and it's styling

--- client/src/components/ReviewsForm.jsx
+++ client/src/components/ReviewsForm.jsx
@@ -1,66 +1,77 @@
 
 
 import React, { useState } from 'react';
-import './ReviewForm.css'
+import './ReviewForm.css';
 
 const ReviewsForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    comment: '',
-    rating: ''
-  });
-  const handleFormChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-  const handleFormSubmit = (e) => {
+  const [message, setMessage] = useState("");
+  const [rating, setRating] = useState(0);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    // Add your logic for handling the form submission here
-    console.log(formData);
-    // Reset the form fields
-    setFormData({
-      name: '',
-      comment: '',
-      rating: ''
-    });
+    // Perform submit logic here
+    console.log("Message:", message);
+    console.log("Rating:", rating);
   };
+
   return (
-    <div>
-      <h2>Add a Review</h2>
-      <form className="form" onSubmit={handleFormSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleFormChange}
-          />
-        </label>
-        <label>
-          Comment:
+    <div
+      style={{
+        backgroundColor: "orange",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "50vh",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          padding: "20px",
+          borderRadius: "10px",
+          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+        }}
+      >
+        <h2 style={{ textAlign: "center", color: "purple" }}>Add Review</h2>
+        <form onSubmit={handleSubmit}>
           <textarea
-            name="comment"
-            value={formData.comment}
-            onChange={handleFormChange}
-          />
-        </label>
-        <label>
-          Rating:
+            name="message"
+            placeholder="Enter your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            style={{
+              width: "200%",
+              height: "200px",
+              resize: "none",
+              marginBottom: "10px",
+            }}
+          ></textarea>
+          <label htmlFor="rating">Rating:</label>
           <input
             type="number"
             name="rating"
             min="1"
             max="5"
-            value={formData.rating}
-            onChange={handleFormChange}
+            value={rating}
+            onChange={(e) => setRating(parseInt(e.target.value))}
+          /><br />
+          <input
+            type="submit"
+            value="Submit"
+            style={{
+              padding: "10px 20px",
+              fontSize: "16px",
+              backgroundColor: "#4CAF50",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
           />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
+
 export default ReviewsForm;
