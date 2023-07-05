c5bd46b37db0ccabe7af5184a9f91ae82ecaede2
gich2009 <waneneamos@gmail.com>
2023-07-05 22:30:35 +0300

    Merged backend and front end v1

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,7 +1,7 @@
 class AirportsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
+  rescue_from ActiveRecord::RecordInvalid,  with: :render_unprocessable_entity_response
+  
   def index
     airports = Airport.all
     render json: airports
