a6421de0536d8509bb54adafc807c3325b4da07c
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-07-05 12:23:02 +0300

    v4 profile navbar

--- client/src/components/Profilenav.jsx
+++ client/src/components/Profilenav.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {NavLink} from 'react-router-dom'
+import {NavLink} from 'react-router-dom';
 
 
 function ProfileNav(){
   return(
-    <div style={{backgroundColor:"orange", display:"flex",alignItems:"center"}}>
-      <div style={{ width: '100px', height: '100px', borderRadius: '50%', overflow: 'hidden' }}>
-  <img src="../Avatar/face.png" alt="Avatar" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-</div>
+    <div style={{backgroundColor:"orange",height:'50px', display:"flex",alignItems:"center"}}>
+     <div style={{ width: '100px', height: '100px', borderRadius: '50%', overflow: 'hidden' }}>
+      <img src="../Avatar/face.png" alt="Avatar" style={{ width: '100%', height: '100%',objectFit: 'cover' }}/>
+    </div>
 
-     
+   
      <nav>
     
    <div style={{ marginLeft: '80vw', display: 'flex', alignItems: 'center' }}>
@@ -17,10 +17,6 @@ function ProfileNav(){
      <NavLink to="/" style={{ textDecoration: 'none', color: 'black', fontWeight: '900', marginRight: '10px' }}>Logout</NavLink>
    </div>
             
-        {/* <div>
-            <Link to="/"> Home </Link>
-            <Link to="/">Log out</Link>
-        </div> */}
      </nav>
     </div>
   );
--- client/src/pages/BookingItem.jsx
+++ client/src/pages/BookingItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
 import Item from "../components/Item";
+import ProfileNav from "../components/Profilenav";
 
 function BookingItem(){
   return(
     <div>
+      <ProfileNav/>
       <Header />
       <Item />
     </div>
