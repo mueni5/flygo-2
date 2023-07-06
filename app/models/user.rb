08c3102c4441e961069a43846e117f879236ceb0
william arasirwa <mageboarasirwa03@gmail.com>
2023-07-06 09:01:21 +0300

    added validatiations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :flights, through: :bookings
   has_secure_password
+
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 4}
+  validates :first_name, presence: true
+  validates :last_name, presence:true
 end
